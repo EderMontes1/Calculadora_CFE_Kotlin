b72eaea0555c2e1b7c24994e077f5c3ef8be5d3e
Ing. Eder Montes <amontesr1@hotmail.com>
2024-05-29 15:41:41 -0600

    Se incorporan widgets. Se incorpora delay en SplashScreen Se incorpora codigo de operacion entre ConsumoFinal y ConsumoInicial

--- app/src/main/java/com/ingedermontes/calculadoracfe/MainActivity.kt
+++ app/src/main/java/com/ingedermontes/calculadoracfe/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.ingedermontes.calculadoracfe
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        runBlocking {
+            // Delay de 3.5 segundos
+            delay(3500)
+
+            // Inicia El segundo Activty despues del delay
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/ingedermontes/calculadoracfe/MainActivity2.kt
+++ app/src/main/java/com/ingedermontes/calculadoracfe/MainActivity2.kt
@@ -1,20 +1,34 @@
 package com.ingedermontes.calculadoracfe
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonCalcular = findViewById<Button>(R.id.buttonCalcular)
+        val consumoFinalKwh = findViewById<TextView>(R.id.textViewConsumoKwhFinal)
+        val eTextAnterior = findViewById<EditText>(R.id.eTextAnterior)
+        val eTextFinal = findViewById<EditText>(R.id.eTextFinal)
+
+
+        buttonCalcular.setOnClickListener {
+            // Cambia el Texto del Consumo en Khw restando (consumoFinal - consumoInicial)
+            val consumoInicial = eTextAnterior.text.toString().toInt()
+            val consumoFinal = eTextFinal.text.toString().toInt()
+            val consumoTotal = consumoFinal - consumoInicial
+
+
+            consumoFinalKwh.text= consumoTotal.toString()
         }
     }
 }
